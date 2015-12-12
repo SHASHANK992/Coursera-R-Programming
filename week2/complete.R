4bfd1b1ab5af1cb7026ed62ffe289f968c8d935a
Shashank Raina <SHASHANKRAINA@LIVE.IN>
2015-12-13 05:17:39 +0530

    Update complete.R

--- week2/complete.R
+++ week2/complete.R
@@ -1,41 +1,51 @@
-setwd("~/Desktop/Online Coursera/Coursera-R-Programming/week2/")
 complete <- function(directory, id = 1:332) {
-    ## 'directory' is a character vector of length 1 indicating
-    ## the location of the CSV files
+  
+  
+  dataframe = NULL  ## initializing the dataframe we want from this function   
+  setwd(file.path(getwd(), directory)) ## setting the directory
+  
+  #Looping thru the directory's files specified in the 'id' argument 
+  for (i in id)
+{
     
-    ## 'id' is an integer vector indicating the monitor ID numbers
-    ## to be used
     
-    ## Return a data frame of the form:
-    ## id nobs
-    ## 1  117
-    ## 2  1041
-    ## ...
-    ## where 'id' is the monitor ID number and 'nobs' is the
-    ## number of complete cases
-    
-    # set working directory
-    if(grep("specdata", directory) == 1) {
-        directory <- ("./specdata/")
-    }
-    # get the length of id vector
-    id_len <- length(id)
-    complete_data <- rep(0, id_len)
-    # find all files in the specdata folder
-    all_files <- as.character( list.files(directory) )
-    file_paths <- paste(directory, all_files, sep="")
-    j <- 1 
-    for (i in id) {
-        current_file <- read.csv(file_paths[i], header=T, sep=",")
-        complete_data[j] <- sum(complete.cases(current_file))
-        j <- j + 1
-    }
-    result <- data.frame(id = id, nobs = complete_data)
-    return(result)
-} 
+      ## Due to the format of the filename, i.e 001, 010  instead of 1, 10. I became aware that the following method works but not efficient, 
+      ## but at the time of the completion of this assignment, it was the only way I knew how to do it.           
+      if (i <10) { 
+                         data <- read.csv(paste("0","0", as.character(i), ".csv", sep=""),  ## for example, if 'id' =7, we get 007.csv
+                                          header = T, 
+                                          na.strings=c("NA","NaN", " "))
+                 }
+      
+else if (i>=10 & i<100) { 
+                         data <- read.csv(paste("0", as.character(i), ".csv", sep=""),  ## for example, if 'id' = 17, we get 017.csv
+                                          header = T, 
+                                          na.strings=c("NA","NaN", " ") 
+                                          )
+                        }
+                     
+       
 
-# tests
-complete("specdata", 1)
-complete("specdata", c(2, 4, 8, 10, 12))
-complete("specdata", 30:25)
-complete("specdata", 3)
+     else       { 
+                        data <- read.csv(paste(as.character(i), ".csv", sep=""),     ## Normal
+                                         header = T, 
+                                         na.strings=c("NA","NaN", " ") 
+                                        )
+                }
+  
+  ## getting rid of all the "NA" values and, consequently, all the non-complete ovservations (the ones with at least one NA in row)
+    data = na.omit(data) 
+ ##  make it a matrix to easily fill each successive row of our dataframe
+    data = as.matrix(data)
+    dataframe = rbind(dataframe, c(i,nrow(data))) # fill each successive row of our dataframe. Each row contains the monitor ID,
+                                                  # and its total complete observed cases (no rows containg NAs)
+    
+    
+    
+  }
+  
+  setwd("..")  # reseting working directory path
+  dataframe = data.frame(dataframe)  # from matix to data frame 
+  names(dataframe) = c('id', 'nobs') # set the column names of the data frame
+  return (dataframe) 
+}
