37b451de9d6646e7ab830b5dac37e52c2bad1706
Shashank Raina <SHASHANKRAINA@LIVE.IN>
2015-12-24 07:38:18 +0530

    Update best.R

--- week4/best.R
+++ week4/best.R
@@ -1,49 +1,24 @@
-setwd("~/Desktop/Online Coursera/Coursera-R-Programming/week4/")
-list.files()
-# helper function for getting the hospital name
-helper <- function(data, col_num, state) {
-    state_subset <- data[data[, 7]==state, ]
-    outcome_arr <- state_subset[, col_num]
-    min <- min(outcome_arr, na.rm=T)
-    min_index <- which(outcome_arr == min)
-    hosp_name <- state_subset[min_index, 2]
-    return(hosp_name)
-}
+## best.R
 
 best <- function(state, outcome) {
-    ## Read outcome data
-    ## Check that state and outcome are valid
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
-    
-    # read the data file
-    directory <- "./data/outcome-of-care-measures.csv"
-    data <- read.csv(directory, colClasses="character")
-    # change data type from character to numeric
-    data[, 11] <- as.numeric(data[, 11]) # heart attack
-    data[, 17] <- as.numeric(data[, 17]) # heart failure
-    data[, 23] <- as.numeric(data[, 23]) # pneumonia
-    valid_outcomes <- c("heart attack", "heart failure", "pneumonia")
-    if (!state %in% data$State) {
-        stop("invalid state")
-    } else if(!outcome %in% valid_outcomes) {
-        stop("invalid outcome")
-    } else {
-        if(outcome == "heart attack") {
-            hosp_name <- helper(data, 11, state)
-        } else if(outcome == "heart failure") {
-            hosp_name <- helper(data, 17, state)
-        } else {
-            hosp_name <- helper(data, 23, state)
-        }
-        result <- hosp_name
-        return(result)
-    }
+  
+  setwd("/Users/shashankraina/desktop/ass3")
+  #print(getwd())
+  data <- read.csv("./outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
+  
+  ## Check that state and outcome are valid
+  validOutcome = c("heart attack","heart failure","pneumonia")
+  if (!outcome %in% validOutcome) { stop("invalid outcome")}
+  
+  validState = unique(data[,7])
+  if (!state %in% validState) stop("invalid state")
+  
+  ## convert outcome name into column name
+  fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+  colName <- fullColName[match(outcome,validOutcome)]
+  
+  ## Return hospital name in that state with lowest 30-day death rate
+  data.state <- data[data$State==state,]
+  idx <- which.min(as.double(data.state[,colName]))
+  data.state[idx,"Hospital.Name"]
 }
-
-# tests
-#best("TX", "heart attack")
-#best("TX", "heart failure")
-#best("MD", "heart attack")
-#best("MD", "pneumonia")
-#best("BB", "heart attack")
